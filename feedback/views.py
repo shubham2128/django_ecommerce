4c3b94b445ba676cbdbeacf75b81a213288bfb65
shubham2128 <shubham.singh280795@gmail.com>
2024-04-08 21:00:01 +0530

    Added feedback app

--- feedback/views.py
+++ feedback/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import FeedbackForm
 
-# Create your views here.
-def submit_feedback(request):
-    return render(request , 'feedback.html')
+@login_required
+def feedback_submission(request):
+    if request.method == 'POST':
+        form = FeedbackForm(request.POST)
+        if form.is_valid():
+            feedback = form.save(commit=False)
+            feedback.user = request.user
+            feedback.save()
+            return redirect('feedback-thanks')  # Redirect to a thank you page
+    else:
+        form = FeedbackForm()
+    return render(request, 'feedback_form.html', {'form': form})
 
+@login_required
+def feedback_thanks(request):
+    return render(request, 'feedback_thanks.html')
